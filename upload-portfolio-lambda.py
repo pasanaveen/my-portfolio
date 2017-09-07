521d3fb17661669d37417c014e9a20e7f2f4cc06
pasa naveen <pasa.naveen@gmail.com>
2017-09-06 20:12:08 -0400

    uploading the python script to build

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
+build_bucket = s3.Bucket('portfoliobuild.corecomet.com')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for name in myzip.namelist():
+        obj = myzip.open(name)
+        portfolio_bucket.upload_fileobj(obj, name,
+            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
