3130e2385cdf89ec64204bb167c5149e5bb1c240
pasa naveen <pasa.naveen@gmail.com>
2017-09-06 23:09:39 -0400

    Commiting the SNS script included in the lambda function

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -6,21 +6,28 @@ import mimetypes
 
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:680058821313:deployPortfolioTopic')
 
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-    portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
-    build_bucket = s3.Bucket('portfoliobuild.corecomet.com')
+        portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
+        build_bucket = s3.Bucket('portfoliobuild.corecomet.com')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-        for name in myzip.namelist():
-            obj = myzip.open(name)
-            portfolio_bucket.upload_fileobj(obj, name,
-                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+            for name in myzip.namelist():
+                obj = myzip.open(name)
+                portfolio_bucket.upload_fileobj(obj, name,
+                    ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+                portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
-    print "job done!"
-    return 'Hello from Lambda'
+        print "job done!"
+    except:
+        topic.publish(Subject="Portfolio failed", Message="Portfolio failed with an exception")
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully")
+    raise
+    return "hello from lambda"
