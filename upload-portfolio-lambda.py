ebfd956e032d5ac2a301d62eed065358d53700f6
pasa naveen <pasa.naveen@gmail.com>
2017-09-06 22:28:25 -0400

    updated upload-portfolio-lambda.py script to automate the code build process

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,23 @@ import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
-build_bucket = s3.Bucket('portfoliobuild.corecomet.com')
+def lambda_handler(event, context):
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for name in myzip.namelist():
-        obj = myzip.open(name)
-        portfolio_bucket.upload_fileobj(obj, name,
-            ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
-        portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+    portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
+    build_bucket = s3.Bucket('portfoliobuild.corecomet.com')
+
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for name in myzip.namelist():
+            obj = myzip.open(name)
+            portfolio_bucket.upload_fileobj(obj, name,
+                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
+            portfolio_bucket.Object(name).Acl().put(ACL='public-read')
+
+    print "job done!"
+    return 'Hello from Lambda'
