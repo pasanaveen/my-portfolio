93599583b5d26cd5778577579d164222b421afbf
pasa naveen <pasa.naveen@gmail.com>
2017-09-08 15:50:44 -0400

    added mimetypes

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,28 +2,45 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
+import mimetypes
+
 
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:680058821313:deployPortfolioTopic')
 
+    location = {
+        "bucketName": 'portfoliobuild.corecome.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+
         s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
         portfolio_bucket = s3.Bucket('portfolio.corecomet.com')
-        build_bucket = s3.Bucket('portfoliobuild.corecome.com')
+        build_bucket = s3.Bucket(location["bucketName"])
 
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
 
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for name in myzip.namelist():
                 obj = myzip.open(name)
-                portfolio_bucket.upload_fileobj(obj, name)
+                portfolio_bucket.upload_fileobj(obj, name,
+                ExtraArgs={'ContentType': mimetypes.guess_type(name)[0]})
                 portfolio_bucket.Object(name).Acl().put(ACL='public-read')
 
         print "Job Done!"
         topic.publish(Subject="Portfolio Success", Message="Portfolio was deployed successfully")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="Portfolio failed", Message="Portfolio failed with an exception")
     raise
