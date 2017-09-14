42cc58a79c7b265f5792797311e2a73bde02684d
pasanaveen <pasa.naveen@gmail.com>
2017-09-14 13:35:12 -0400

    Updated with github and soundcloud URL's and Description

--- js/main.js
+++ js/main.js
@@ -6,23 +6,14 @@ const myWork = [
   {
     'title': "Work Example 1 - Serverless using AWS",
     'href': "https://github.com/pasanaveen/my-portfolio",
-    'desc': `"Build a complete Portfolio website using AWS technologies. 
-    - Serverless programming is a great way to build websites without using any native servers. 
+    'desc': Build a complete Portfolio website using AWS technologies. 
+    Serverless programming is a great way to build websites without using any native servers. 
+    The technologies used in this project are : 
+     AWS S3, AWS Lambda, Code Build, Cloud Front, IAM, Code Pipeline, Python & boto3, GitHub, HTML, CSS, Babel, ReactJS, Web Pack 
+     Jest, Enzyme, Chai , Mocha, Node Js and NPM.
     
-    - The technologies used in this project are : 
-    1. AWS S3 ( For storing Objects, Images, Frontend code related files etc.. ).
-    2. AWS Lambda ( For invoking the Lambda function in AWS to deploy the code ).
-    3. Code Build ( For automatic build ).
-    4. Cloud Front ( For distribution of the website to other regions ).
-    5. IAM ( For security and credentials ).
-    6. Code Pipeline ( For automating entire code build, deploy process).
-    7. Python & boto3 ( For writing scripts for Lambda function).
-    8. GitHub ( Used as Code Repository).
-    9. HTML, CSS, Babel ( For maintaining compatability across browsers ).
-    10. ReactJS ( React Js framework for making the website dynamic and interactive ).
-    11. Web Pack ( For packaging the files and distributing the code in proper manner ).
-    12. Jest, Enzyme, Chai , Mocha ( For making responsive and Testing the code).
-    13. Node Js and NPM."`,
+    Click on "CHECK IT OUT" button to learn more about this."
+    ",
     'image': {
       'desc': "example screenshot of a project involving chemistry",
       'src': "images/example1.PNG",
@@ -32,8 +23,11 @@ const myWork = [
 
   {
     'title': "Work Example 2",
-    'href': "https://example.com",
-    'desc': "Pending.....",
+    'href': "https://soundcloud.com/naveen-pa/sets/soundcomet-2017",
+    'desc': Check out my first Album "soundcomet-2017" produced and composed by me!..
+            The music is produced and composed using LoopPacks and LaunchPad. 
+  
+             Click on "CHECK IT OUT" button!...",
     'image': {
       'desc': "example screenshot of a project involving cats",
       'src': "images/example1.PNG",
